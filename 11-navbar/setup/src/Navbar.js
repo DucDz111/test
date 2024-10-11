1e00de2a5c4f183eed484e5f166f06ab77d32251
DucDz111 <19130047@st.hcmuaf.edu.vn>
2024-10-11 14:03:02 +0700

    Your commit message here

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,55 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showsLink, setShowsLink] = useState(false)
+  return (
+    <nav>
+      <div className="nav-center">
+        l <div className="nav-header">
+             <img src={logo} alt="logo"/>
+             <button className="nav-toggle" onClick={()=>setShowsLink(!showsLink)}>
+               <FaBars/>
+             </button>
+        </div>
+        {showsLink && <div className="links-container show-container">
+            <ul className="links">
+              {links.map((link)=>{
+                const {id,url,text} =link
+                return (
+                  <li key ={id}>
+                    <a href ={url}>{text}</a>
+                  </li>
+                )
 
-export default Navbar
+
+              })}
+            </ul>
+          </div>}
+       
+          <ul className="social-icons">
+            <li>
+              <a href="https://x.com/">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/">
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/">
+                <FaTwitter />
+              </a>
+            </li>
+
+          </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
